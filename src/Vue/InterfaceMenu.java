54b75498839bc3625baf7f3ec9db1670a5f51522
tom7438 <tom192010@live.fr>
2022-04-30 00:16:30 +0200

    Début avec interface Menu et interface jeu

--- src/Vue/InterfaceMenu.java
+++ src/Vue/InterfaceMenu.java
@@ -1,6 +1,9 @@
 package Vue;
 
 import javax.swing.*;
+import javax.swing.event.MouseInputAdapter;
+
+import java.awt.*;
 import java.awt.event.*;
 
 public class InterfaceMenu extends InterfaceGraphique {
@@ -10,48 +13,53 @@ public class InterfaceMenu extends InterfaceGraphique {
         this.frame.setSize(400, 400);
         this.frame.setLocationRelativeTo(null);
         this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.frame.setMinimumSize(new Dimension(400,400));
         this.frame.setVisible(true);
         
-        Box box = Box.createVerticalBox();
-        JButton test = new JButton("jouer");
+        Box boxFinal = Box.createVerticalBox();
+        JButton play = createButton("Play");
+        JButton load = createButton("Load game");
+        JButton exit = createButton("Exit");
 
-        // tests
-        // todo: implémenter le controleur pour souris etc
-        test.addMouseListener(new MouseListener(){
 
-            @Override
+        play.addMouseListener(new MouseInputAdapter() {
             public void mouseClicked(MouseEvent e) {
+                // Passe à l'interface Jeu
                 new InterfaceJeu();
                 fermer();
             }
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e) {
-                // TODO Auto-generated method stub
-                
-            }
-            
         });
-        box.add(test);
-        this.frame.add(box);
+        Box box1 = Box.createHorizontalBox();
+        box1.add(play);
+        boxFinal.add(box1);
+        
+
+        load.addMouseListener(new MouseInputAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                // TODO
+            }
+        });
+        Box box2 = Box.createHorizontalBox();
+        box2.add(load);
+        boxFinal.add(box2);
+        
+
+        exit.addMouseListener(new MouseInputAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                // Ferme la fenêtre
+                fermer();
+            }
+        });
+        Box box3 = Box.createHorizontalBox();
+        box3.add(exit);
+        boxFinal.add(box3);
+        
+        
+        
+
+        this.frame.add(boxFinal);
+
+
     }
 
     public void fermer() {

a8fd023eb63fc9994048e8ce22cea46683d17b9a
tom7438 <tom192010@live.fr>
2022-04-29 19:48:14 +0200

    MAJ organisaiton IHM

--- src/Gaufre.java
+++ src/Gaufre.java
@@ -1,7 +1,7 @@
-import Vue.InterfaceGraphique;
+import Vue.InterfaceMenu;
 
 public class Gaufre {
     public static void main(String[] args) throws Exception {
-        InterfaceGraphique.demarre();
+        new InterfaceMenu();
     }
 }

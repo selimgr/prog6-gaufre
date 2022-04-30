d1c637504d90b65ce1e546a2637778ebdfcd02c5
dcs-b <81160533+dcs-b@users.noreply.github.com>
2022-04-30 18:03:27 +0200

    Améliorations Jeu

--- src/Modele/Jeu.java
+++ src/Modele/Jeu.java
@@ -3,6 +3,7 @@ package Modele;
 public class Jeu {
     Niveau n;
     boolean partieCommencee;
+    boolean partieTerminee;
 
     /**
      * Crée une nouvelle partie de taille par défaut
@@ -13,12 +14,14 @@ public class Jeu {
 
     /**
      * Crée une nouvelle partie de taille choisie
-     * @param l : nombre de lignes
-     * @param c : nombre de colonnes
+     * @param l nombre de lignes
+     * @param c nombre de colonnes
+     * @throws IllegalArgumentException si l < 1 ou c < 1
      */
     public void nouvellePartie(int l, int c) {
         n = new Niveau(l, c);
         partieCommencee = false;
+        partieTerminee = false;
     }
 
     /**
@@ -30,8 +33,12 @@ public class Jeu {
 
     /**
      * Ajoute une nouvelle ligne
+     * @throws IllegalStateException si aucun appel à nouvellePartie n'a été effectué
      */
     public void ajouterLigne() {
+        if (n == null) {
+            throw new IllegalStateException("Aucun niveau auquel ajouter une ligne");
+        }
         if (!partieCommencee) {
             n.ajouterLigne();
         }
@@ -39,17 +46,25 @@ public class Jeu {
 
     /**
      * Supprime une ligne
+     * @throws IllegalStateException si aucun appel à nouvellePartie n'a été effectué
      */
     public void supprimerLigne() {
-        if (!partieCommencee) {
+        if (n == null) {
+            throw new IllegalStateException("Aucun niveau auquel supprimer une ligne");
+        }
+        if (!partieCommencee && n.lignes() > 1) {
             n.supprimerLigne();
         }
     }
 
     /**
      * Ajoute une nouvelle colonne
+     * @throws IllegalStateException si aucun appel à nouvellePartie n'a été effectué
      */
     public void ajouterColonne() {
+        if (n == null) {
+            throw new IllegalStateException("Aucun niveau auquel ajouter une colonne");
+        }
         if (!partieCommencee) {
             n.ajouterColonne();
         }
@@ -57,36 +72,50 @@ public class Jeu {
 
     /**
      * Supprime une colonne
+     * @throws IllegalStateException si aucun appel à nouvellePartie n'a été effectué
      */
     public void supprimerColonne() {
-        if (!partieCommencee) {
+        if (n == null) {
+            throw new IllegalStateException("Aucun niveau auquel supprimer une colonne");
+        }
+        if (!partieCommencee && n.colonnes() > 1) {
             n.supprimerColonne();
         }
     }
 
     /**
      * Joue un coup
-     * @param l : indice de la ligne du coup à jouer
-     * @param c : indice de la colonne du coup à jouer
+     * @param l indice de la ligne du coup à jouer
+     * @param c indice de la colonne du coup à jouer
+     * @return true si un coup a été joué et false sinon
+     * @throws IllegalStateException si aucun appel à nouvellePartie n'a été effectué
+     * @throws IndexOutOfBoundsException si l ou c est en dehors des dimensions du niveau
      */
-    public void coup(int l, int c) {
+    public boolean coup(int l, int c) {
+        if (n == null) {
+            throw new IllegalStateException("Aucun niveau auquel jouer");
+        }
         partieCommencee = true;
 
         if (!n.coup(l, c)) {
-            return;
+            return false;
         }
 
-        if (l == 0 && c == 0) {
-            partieTermine();
+        if (n.estTermine()) {
+            partieTerminee = true;
         } else {
             joueurSuivant();
         }
+        return true;
     }
 
-    void partieTermine() {
-
+    /**
+     * @return true si la partie est terminée et false sinon
+     */
+    public boolean partieTerminee() {
+        return partieTerminee;
     }
-
+    
     void joueurSuivant() {
 
     }

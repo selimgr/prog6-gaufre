e2fb604663de85b53a6f5e3a533e24a7613d4c7a
selimgr <jaawis.dev@gmail.com>
2022-05-03 19:57:12 +0200

    fix placeholder

--- src/Vue/InterfaceGraphique.java
+++ src/Vue/InterfaceGraphique.java
@@ -23,8 +23,7 @@ abstract class InterfaceGraphique implements Runnable {
 
     JFrame frame;
 
-
-    public JButton createButton(String s){
+    public JButton createButton(String s) {
         JButton button = new JButton(s);
 		button.setAlignmentX(Component.TOP_ALIGNMENT);
 		button.setPreferredSize(new Dimension(120, 50));
@@ -32,20 +31,26 @@ abstract class InterfaceGraphique implements Runnable {
         return button;
     }
 
-    public JTextField createText(String s){
+    public JTextField createText(String s) {
         JTextField T1 = new JTextField(s, 50);
         T1.getFont().deriveFont(Font.ITALIC);
         T1.setForeground(Color.gray);
-        T1.addMouseListener(new MouseInputAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                // Si le player clique, alors le texte s'efface
-                JTextField texte = ((JTextField)e.getSource());
-                texte.setText("");
-                texte.getFont().deriveFont(Font.PLAIN);
-                texte.setForeground(Color.black);
-                texte.removeMouseListener(this);
+        T1.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (T1.getText().equals(s)) {
+                    T1.setText("");
+                    T1.setForeground(Color.BLACK);
+                }
             }
-        });
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (T1.getText().isEmpty()) {
+                    T1.setForeground(Color.GRAY);
+                    T1.setText(s);
+                }
+            }
+            });
         return T1;
     }
 

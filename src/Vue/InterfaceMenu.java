1e2221729a206b403bb9f1303bbc67a431b24c65
selimgr <jaawis.dev@gmail.com>
2022-05-04 00:05:02 +0200

    petit fix qui sera utile

--- src/Vue/InterfaceMenu.java
+++ src/Vue/InterfaceMenu.java
@@ -57,13 +57,13 @@ public class InterfaceMenu extends InterfaceGraphique {
         GroupJ1 = new ButtonGroup();
         b1 = new JRadioButton[4];
         b1[0] = new JRadioButton("Human", false);
-        b1[0].setActionCommand("true");
+        b1[0].setActionCommand("Human");
         b1[1] = new JRadioButton("Easy AI", true);
-        b1[1].setActionCommand("false");
+        b1[1].setActionCommand("Easy AI");
         b1[2] = new JRadioButton("Medium AI", false);
-        b1[2].setActionCommand("false");
+        b1[2].setActionCommand("Medium AI");
         b1[3] = new JRadioButton("Difficult AI", false);
-        b1[3].setActionCommand("false");
+        b1[3].setActionCommand("Difficult AI");
         GroupJ1.add(b1[0]);GroupJ1.add(b1[1]);
         GroupJ1.add(b1[2]);GroupJ1.add(b1[3]);
 
@@ -87,13 +87,13 @@ public class InterfaceMenu extends InterfaceGraphique {
         GroupJ2 = new ButtonGroup();
         b2 = new JRadioButton[4];
         b2[0] = new JRadioButton("Human", false);
-        b2[0].setActionCommand("true");
+        b2[0].setActionCommand("Human");
         b2[1] = new JRadioButton("Easy AI", true);
-        b2[1].setActionCommand("false");
+        b2[1].setActionCommand("Easy AI");
         b2[2] = new JRadioButton("Medium AI", false);
-        b2[2].setActionCommand("false");
+        b2[2].setActionCommand("Medium AI");
         b2[3] = new JRadioButton("Difficult AI", false);
-        b2[3].setActionCommand("false");
+        b2[3].setActionCommand("Difficult AI");
         GroupJ2.add(b2[0]);GroupJ2.add(b2[1]);
         GroupJ2.add(b2[2]);GroupJ2.add(b2[3]);
 
@@ -167,11 +167,11 @@ public class InterfaceMenu extends InterfaceGraphique {
     // Permet de récupérer le Joueur/IA
     public Joueur getJ1() {
         if (T1.getText().compareTo("Name of Player 1") == 0) return null;
-        return new Joueur(T1.getText(), GroupJ1.getSelection().getActionCommand() == "true" ? false : true);
+        return new Joueur(T1.getText(), GroupJ1.getSelection().getActionCommand() == "Human" ? false : true);
     }
 
     public Joueur getJ2(){
         if (T2.getText().compareTo("Name of Player 2") == 0) return null;
-        return new Joueur(T2.getText(), GroupJ2.getSelection().getActionCommand() == "true" ? false : true);
+        return new Joueur(T2.getText(), GroupJ2.getSelection().getActionCommand() == "Human" ? false : true);
     }
 }
